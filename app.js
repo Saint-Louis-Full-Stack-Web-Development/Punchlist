1e9be4ab305ab04f14b84e8241716120c3d3e4d0
cschmitz <schmitz.chris@gmail.com>
2016-04-13 13:11:36 -0500

    Adds store to interact with local storage. Modifies watch on app to work for edits.
    The store idea is derived from the todomvc in the vue source code. It’s
    “simplified” in this project because I’m trying to keep this as flat as
    I can to prove a point during the meetup.

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
+var localStore = function (key){
+    this.KEY = key;
+}
+
+localStore.prototype.fetch = function (){
+    return JSON.parse(localStorage.getItem(this.KEY)) || [];
+}
+
+localStore.prototype.save = function (todos){
+    localStorage.setItem(this.KEY, JSON.stringify(todos));
+}
+
+var store = new localStore('punchlist');
+
+
 new Vue({
     el: 'body',
     data:{
-        punchlist: [
-            { description: 'Build list', complete: false, editing: false },
-            { description: 'Test list', complete: false, editing: false },
-        ],
-        newTask: ''
+        punchlist: store.fetch(),
+        // [
+            // { description: 'Build list', complete: false, editing: false },
+            // { description: 'Test list', complete: false, editing: false },
+        // ],
+        newTask: '',
+        editCache: ''
+    },
+    watch: {
+        punchlist:{
+            handler: function (punchlist){
+                store.save(punchlist);
+            },
+            deep: true
+        }
     },
     methods: {
         toggleComplete: function (task){
             task.complete = !task.complete;
         },
-        toggleEdit: function (task){
-            task.editing = !task.editing;
-            task.description === '' ? this.punchlist.$remove(task) : null
+        startEditing: function (task){
+            task.editing = true;
+        },
+        endEditing: function (task){
+            task.editing = false;
+            // task.description = task.description.trim();
+            if(task.description === ''){
+                this.punchlist.$remove(task);
+            }
         },
         addTask: function (){
             if(this.newTask !== ''){
